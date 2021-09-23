c9b45e264756e85604608aabb9a3b1af47dcf9ff
ritz89 <zetsubou.harits@gmail.com>
2021-09-24 02:41:54 +0800

    new template

--- commerce/admin.py
+++ commerce/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from import_export.admin import ImportExportModelAdmin
+
+from commerce.models import Item, OrderItem, Order, ItemCategory, MediaLibrary
+
+
+class ItemAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = [f.name for f in Item._meta.fields]
+    list_filter = ['title', ]
+    search_fields = [f.name for f in Item._meta.fields]
+
+
+admin.site.register(Item, ItemAdmin)
+
+admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(ItemCategory)
+admin.site.register(MediaLibrary)
